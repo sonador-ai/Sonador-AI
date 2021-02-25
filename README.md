f6c09676857b0a598de0279b9b0d3240424c9742
Ethan Chapman <echapman@oak-tree.us>
2021-02-25 07:48:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sonador AI
+
+Tools for creating machine learning and artificial intelligence applications using medical imaging data from Sonador.
+
+## Installation
+
+
+```bash
+pip3 install 
+```
+
+
+## Usage
+
+```py
+import sonadorai as ai
+```
+
---
+++ sonadorai/data/tensorflow.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow.keras.utils import Sequence
+
+
+class DataLoader(Sequence):
+    '''Sequence of data loaded from Sonador
+    # Arguments
+        layers: list of layers to add to the model.
+    # Note
+        The first layer passed to a Sequential model
+        should have a defined input shape. What that
+        means is that it should have received an `input_shape`
+        or `batch_input_shape` argument,
+        or for some type of layers (recurrent, Dense...)
+        an `input_dim` argument.
+    # Example
+        ```python
+            pass
+        ```
+    '''
+
+    def __init__(self, x_set, y_set, batch_size):
+        self.x, self.y = x_set, y_set
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return math.ceil(len(self.x) / self.batch_size)
+
+    def __getitem__(self, idx):
+        batch_x = self.x[idx * self.batch_size:(idx + 1) *
+        self.batch_size]
+        batch_y = self.y[idx * self.batch_size:(idx + 1) *
+        self.batch_size]
+
+        return np.array([
+            resize(imread(file_name), (200, 200))
+               for file_name in batch_x]), np.array(batch_y)
